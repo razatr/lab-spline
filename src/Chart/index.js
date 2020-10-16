18ee246d517326030a8ec4db347a39bdf54f6145
Legolas <razatr@yandex.ru>
2020-10-16 15:27:23 +0300

    initial commit

---
+++ src/Chart/index.js
@@ -0,0 +1,50 @@
+import React, { Fragment } from 'react'
+import { VictoryChart, VictoryLine, VictoryTheme, VictoryContainer, VictoryLegend } from 'victory'
+
+function Chart() {
+
+    return (<Fragment>
+        <VictoryChart
+            maxDomain={ { x: 7, y: 4 } }
+            minDomain={ { x: -1, y: -4 } }
+            theme={ VictoryTheme.material }
+            containerComponent={ <VictoryContainer responsive={ false }/> }
+            width={ 800 }
+            height={ 800 }>
+            <VictoryLegend x={ 200 } y={ 50 }
+                           orientation="horizontal"
+                           gutter={ 20 }
+                           data={ [
+                               {
+                                   name: 'Graph of a function',
+                                   symbol: { fill: 'd00f50' },
+                                   labels: { fill: 'd00f50' }
+                               },
+                               {
+                                   name: 'Graph of a Newton polynomial',
+                                   symbol: { fill: '4256b1' },
+                                   labels: { fill: '4256b1' }
+                               },
+                           ] }
+            />
+            <VictoryLine data={ func }
+                         animate={ { duration: 1500 } }
+                         style={ {
+                             data: {
+                                 stroke: '#d00f50'
+                             }
+                         } }/>
+            <VictoryLine data={ newton }
+                         animate={ {
+                             duration: 1500
+                         } }
+                         style={ {
+                             data: {
+                                 stroke: '#4256b1'
+                             }
+                         } }/>
+        </VictoryChart>
+    </Fragment>)
+}
+
+export default Chart
