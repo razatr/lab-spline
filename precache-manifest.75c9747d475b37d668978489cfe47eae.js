cd2631234871781425b8183abfb010c1ab997f77
Legolas <razatr@yandex.ru>
2020-10-22 19:34:21 +0300

    Updates

---
+++ precache-manifest.75c9747d475b37d668978489cfe47eae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3656887bf6718333b2c074a4df7202c",
+    "url": "/lab-spline/index.html"
+  },
+  {
+    "revision": "9d484b948b167b3a23e4",
+    "url": "/lab-spline/static/js/2.dac3d163.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/lab-spline/static/js/2.dac3d163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74d4027931b825bb5b2",
+    "url": "/lab-spline/static/js/main.aa04feef.chunk.js"
+  },
+  {
+    "revision": "bf4da0cd233e93144618",
+    "url": "/lab-spline/static/js/runtime-main.9aaa66a4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lab-spline/precache-manifest.75c9747d475b37d668978489cfe47eae.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lab-spline/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
